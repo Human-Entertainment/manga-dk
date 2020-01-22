82f6a9813e2084389d86d413cd13e324f6484fa1
Bastian Inuk Christensen <bastian@inuk.blog>
2020-01-22 23:33:29 +0100

    Moved partners out of underConstruction, renamed index(req:) to construction(req:) for better clarity

--- Sources/App/Controllers/Index.swift
+++ Sources/App/Controllers/Index.swift
@@ -3,11 +3,14 @@ import Plot
 
 struct Index: RouteCollection {
 	func boot(routes: RoutesBuilder) throws {
-        routes.get("", use: index)
+        routes.get("", use: construction)
 	}
 
-    func index(req: Request) throws -> EventLoopFuture<HTML> {
-        Views.underConstruction.encodeResponse(for: req)
+    func construction(req: Request) throws -> EventLoopFuture<HTML> {
+        // TODO: Make this into a fluent model to make it more dynamic
+        let mwr = Partners(img: "https://mangaworthreading.co/wp-content/uploads/2019/09/Header-Main@2x.png", link: "https://mangaworthreading.co/")
+        let sif = Partners(img: "https://i0.wp.com/switchindiefix.com/wp-content/uploads/2018/09/cropped-logo-text-red.png", link: "http://switchindiefix.com")
+        return Views.underConstruction(sif, mwr).encodeResponse(for: req)
 	}
     
 }
