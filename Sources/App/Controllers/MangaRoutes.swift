75960e2e8c5608c91e017f23519cc43b46f3dd79
Bastian Inuk Christensen <bastian@inuk.blog>
2020-01-24 11:20:18 +0100

    Made manga routes to create manga

--- Sources/App/Controllers/MangaRoutes.swift
+++ Sources/App/Controllers/MangaRoutes.swift
@@ -5,4 +5,16 @@
 //  Created by Bastian Inuk Christensen on 23/01/2020.
 //
 
-import Foundation
+import Vapor
+
+extension Manga: RouteCollection {
+    func boot(routes: RoutesBuilder) throws {
+        let mangaCollection = routes.grouped("manga")
+        mangaCollection.post("new", use: createManga)
+    }
+    
+    func createManga(req: Request) throws -> EventLoopFuture<Manga> {
+        let manga = try req.content.decode(Manga.self)
+        return manga.create(on: req.db).map{ manga }
+    }
+}
