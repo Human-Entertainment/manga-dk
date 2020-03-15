ed7b90e6a255b5841b0675d3775c0bd667e44ed3
Bastian Inuk Christensen <bastian@inuk.blog>
2020-03-15 11:48:54 +0100

    Uptaded to newest dependencies

--- Package.swift
+++ Package.swift
@@ -1,26 +1,32 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "Manga",
     platforms: [
-       .macOS(.v10_14)
+       .macOS(.v10_15)
+    ],
+    products: [
+        .executable(name: "Run", targets: ["Run"]),
+        .library(name: "App", targets: ["App"]),
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-beta"),
-        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-beta"),
-        .package(url: "https://github.com/JohnSundell/Plot.git", from: "0.5.0")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc.3.4"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc.1"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-rc.1"),
+        // 🖥 The view to use on the website, framework made by John Sundell
+        .package(url: "https://github.com/JohnSundell/Plot.git", from: "0.7.0")
     ],
     targets: [
         .target(name: "App", dependencies: [
-            "Fluent", 
-            "FluentPostgresDriver",
-            "Vapor",
+            .product(name: "Fluent", package: "fluent"),
+            .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
+            .product(name: "Vapor", package: "vapor"),
             "Plot"
         ]),
         .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App", "XCTVapor"])
+        .testTarget(name: "AppTests", dependencies: ["App",
+                                                     .product(name: "XCTVapor", package: "vapor")])
     ]
 )
